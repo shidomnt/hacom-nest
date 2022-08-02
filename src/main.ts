2ce32820d1d6258fbd83fe1df86d7dda49dac08b
shidomnt <hatranquang02@gmail.com>
2022-08-02 17:32:10 +0700

    deploy

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: 'https://hacom-123.netlify.app/',
   });
   await app.listen(5000);
 }
