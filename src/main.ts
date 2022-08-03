4c60fb1e561dd04cfe2f328d0f70d8199f9ca115
shidomnt <hatranquang02@gmail.com>
2022-08-03 10:12:23 +0700

    fix merge

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { RoleGuard } from './guards/role.guard';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://hacom-123.netlify.app',
+    origin: process.env.ORIGIN || 'https://hacom-123.netlify.app',
   });
   app.useGlobalGuards(new RoleGuard(new Reflector(), new ConfigService()));
   await app.listen(process.env.PORT || 5000);
